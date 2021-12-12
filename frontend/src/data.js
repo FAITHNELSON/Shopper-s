7793c68509f80d273f6e6d5607f72f183ca557a7
faithnelson <mailimeiniobong@gmail.com>
2021-12-12 12:18:44 +0100

    share code in github

---
+++ frontend/src/data.js
@@ -0,0 +1,116 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Casual shirt for men',
+            category: 'shirts',
+            image: '/images/shirt-1.jpg',
+            price: 19.999,
+            brand: 'Casual',
+            rating: 4.3,
+            numReviews: 11,
+            description: 'high quality product, made with love from us to you.'
+        },
+        {
+            _id: '2',
+            name: 'Red shoe',
+            category: 'shoes',
+            image: '/images/shoe-2.jpeg',
+            price: 462.99,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 40,
+            description: 'high quality product, made with love from us to you.'
+        },
+        {
+            _id: '3',
+            name: 'Handbag',
+            category: 'Bags',
+            image: '/images/bag-1.jpeg',
+            price: 67.99,
+            brand: 'Lady',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'high quality product, made with love from us to you.'
+        },
+        
+        {
+            _id: '4',
+            name: ' Men blue Shoe',
+            category: 'shoes',
+            image: '/images/shoe-3.jpg',
+            price: 89.99,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 29,
+            description: 'high quality product, made with love from us to you.'
+        },
+        {
+            _id: '5',
+            name: 'Green dress',
+            category: 'Dress',
+            image: '/images/lady-3.jpg',
+            price: 90.99,
+            brand: 'Lady',
+            rating: 4.7,
+            numReviews: 17,
+            description: 'high quality product, made with love from us to you.'
+        },
+        {
+            _id: '6',
+            name: 'Dennislingo shirt',
+            category: 'shirts',
+            image: '/images/shirt-2.jpg',
+            price: 100.99,
+            brand: 'Casual',
+            rating: 4.9,
+            numReviews: 14,
+            description: 'high quality product, made with love from us to you.'
+        },
+        {
+            _id: '7',
+            name: 'ladies handbag',
+            category: 'Bags',
+            image: '/images/bag-3.jpg',
+            price: 43.199,
+            brand: 'Lady',
+            rating: 4.1,
+            numReviews: 20,
+            description: 'high quality product, made with love from us to you.'
+        },
+        {
+            _id: '8',
+            name: 'Red dress',
+            category: 'dress',
+            image: '/images/lady-1.jpg',
+            price: 78.99,
+            brand: 'Lady',
+            rating: 4.5,
+            numReviews: 19,
+            description: 'high quality product, made with love from us to you.'
+        },
+        {
+            _id: '9',
+            name: 'unsex shoe',
+            category: 'shoes',
+            image: '/images/shoe-5.jpg',
+            price: 456.99,
+            brand: 'FILA',
+            rating: 4.7,
+            numReviews: 16,
+            description: 'high quality product, made with love from us to you.'
+        },
+        {
+            _id: '10',
+            name: 'V-cross shirt',
+            category: 'shirts',
+            image: '/images/shirt-3.jpg',
+            price: 239.99,
+            brand: 'Casual',
+            rating: 4.8,
+            numReviews: 12,
+            description: 'high quality product, made with love from us to you.'
+        },
+    ],
+};
+export default data;
